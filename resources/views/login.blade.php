d8e9c3ef8a38f98526475da5d2331397e91de4f4
Beniro <beniro_vielma@hotmail.com>
2018-07-18 15:31:37 -0500

    commit to heroku laravel

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -27,7 +27,7 @@
         <p class="login-box-msg"> {{ trans('adminlte_lang::message.siginsession') }} </p>
         <form action="{{ url('/getUser') }}" method="post">
             <!-- <input type="hidden" name="_token" value="{{ csrf_token() }}"> -->
-            {{csrf_field ()}}
+            {!! csrf_field() !!}
             <div class="form-group has-feedback">
                 <input type="email" class="form-control" placeholder="{{ trans('adminlte_lang::message.email') }}" name="email"/>
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
