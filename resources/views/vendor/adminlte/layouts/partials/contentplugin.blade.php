d17a2f9c9b4f202de0168197065ac53f616c443f
Beniro <beniro_vielma@hotmail.com>
2018-07-19 11:37:50 -0500

    commit to heroku laravel

--- resources/views/vendor/adminlte/layouts/partials/contentplugin.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/contentplugin.blade.php
@@ -1,4 +1,5 @@
 <!-- Bootstrap notify libraries -->
 
+<link href="{{ secure_asset('/plugins/bootstrap-notify/notify.css') }}" rel="stylesheet" />
 <script src="{{ secure_asset('/plugins/bootstrap-notify/bootstrap-notify.js') }}"></script>
 <script src="{{ secure_asset('/js/Util.js') }}"></script>
