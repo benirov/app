94b1689330ff361e41a67bda4692dfaf19cb02e6
Beniro <beniro_vielma@hotmail.com>
2018-07-23 11:36:25 -0500

    commit to heroku project

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -45,7 +45,7 @@ class LoginController extends Controller
       $this->validate($request, $rules);
       $fields = $request->all();
       // $fields['password'] =  bcrypt($request->password);
-      $login = DB::select('call sp_getUser(?, ?)', array($fields['email'], $fields["password"]))->first();
+      $login = DB::select('call sp_getUser(?, ?)', array($fields['email'], $fields["password"]));
       // return $login;
       if($login){
         $request->session()->put('User',2);
@@ -53,7 +53,7 @@ class LoginController extends Controller
         $request->session()->put('nameUser',1);
         $request->session()->put('emailUser',1);
 
-        echo $login->email;
+        echo $login[0]->email;
         // return redirect('/home');
          // return redirect()->action('HomeController@getHome');
       }
