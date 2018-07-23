1177236c1cd1abe05ecc4332ff78acd5db78b53c
Beniro <beniro_vielma@hotmail.com>
2018-07-23 18:08:05 -0500

    commit to heroku project

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
         
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="{{  }}" class="img-circle" alt="User Image" />
+                    <img src="/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
                 </div>
                 <div class="pull-left info">
                     <p>{{ Session::get('nameUser') }}</p>
