d7477e0b122640191211030506ed7e28c015c50f
Beniro <beniro_vielma@hotmail.com>
2018-07-24 13:39:45 -0500

    commit to heroku project

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\DB;
 use App\master;
 
 class User extends Authenticatable
