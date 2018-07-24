953bbcf38691f829a2595ffef62c4c95b9541826
Beniro <beniro_vielma@hotmail.com>
2018-07-24 14:46:31 -0500

    commit to heroku project

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -146,7 +146,7 @@ class UserController extends ApiController
          $userMenu = DB::table('tblUserMenu')->whereIn('id', function($query)
             {
                 $query->select(DB::raw('fkIdMenu'))->from('tblProfileUser')->whereRaw('tblProfileUser.fkIdMd', '=', Session::get("Master"));
-            })->get();
+            })->first();
 
          // return $userMenu; 
     }
