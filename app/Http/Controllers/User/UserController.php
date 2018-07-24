c4b4d498e8e0444700dc137c50b739bae1389c71
Beniro <beniro_vielma@hotmail.com>
2018-07-24 15:18:26 -0500

    commit to heroku project

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -146,7 +146,7 @@ class UserController extends ApiController
         echo Session::get("Master");
          $userMenu = DB::table('tblUserMenu')->whereIn('id', function($query)
             {
-                $query->select(DB::raw('fkIdMenu'))->from('tblProfileUser')->whereRaw('tblProfileUser.fkIdMd', '=', 1);
+                $query->select(DB::raw('fkIdMenu'))->from('tblProfileUser')->whereRaw('tblProfileUser.fkIdMd = {Session::get("Master")}');
             })->toSql();
          dd($userMenu);
 
