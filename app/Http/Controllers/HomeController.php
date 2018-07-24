ab693d3c602c69c2abcf868e7f111315e243dfeb
Beniro <beniro_vielma@hotmail.com>
2018-07-24 12:49:33 -0500

    commit to heroku project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -48,6 +48,6 @@ class HomeController extends Controller
 
     public function getHome(){
         // $menu = User::getUserMenu();
-      return view('/home')->with('menu', User::getUserMenu());
+      return view('/home')->with('menu', UserController::getUserMenu());
     }
 }
