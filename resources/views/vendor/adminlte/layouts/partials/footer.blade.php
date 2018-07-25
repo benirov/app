b32ccdd4f99bc091621cccb88a7bcac9144a3beb
Beniro <beniro_vielma@hotmail.com>
2018-07-25 18:02:33 -0500

    commit heroku laravel

--- resources/views/vendor/adminlte/layouts/partials/footer.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -2,14 +2,14 @@
 <footer class="main-footer">
 	<!-- div Loader -->
 	<div id="modalLoader" class="container-modal hidden">
-    <center>
-      <div class="content-modal">
-        <div class="shape shape-1"></div>
-        <div class="shape shape-2"></div>
-        <div class="shape shape-3"></div>
-        <div class="shape shape-4"></div>
-      </div>
-    </center>
+    	<center>
+	      <div class="content-modal">
+	        <div class="shape shape-1"></div>
+	        <div class="shape shape-2"></div>
+	        <div class="shape shape-3"></div>
+	        <div class="shape shape-4"></div>
+	      </div>
+	    </center>
   </div>
 
 
