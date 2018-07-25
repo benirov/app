5893a1c38d4bb60e00d839e79fecc30a7bbc8b90
Beniro <beniro_vielma@hotmail.com>
2018-07-25 10:36:53 -0500

    commit heroku laravel

---
+++ app/Http/Middleware/verifySession.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\contracts\Auth\Guard;
+
+class verifySession
+{
+
+    private $auth;
+
+    public function __construc(Guard $auth){
+        $this->auth =$auth;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        dd($this->auth->user());
+        return $next($request);
+    }
+}
