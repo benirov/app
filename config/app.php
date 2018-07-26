b75144fb2a9d3553360f459dba789d697040c05e
Beniro <beniro_vielma@hotmail.com>
2018-07-26 10:37:34 -0500

    commit heruko laravel

--- config/app.php
+++ config/app.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'CEST',
+    'timezone' => 'UTC',
 
     /*
     |--------------------------------------------------------------------------
@@ -177,6 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
     ],
 
