f39bd4cf44dd14ace0b9ab6db28d063976c711db
Beniro <beniro_vielma@hotmail.com>
2018-07-25 21:44:07 -0500

    commit heroku laravel

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
-// header("Access-Control-Allow-Origin: https://young-coast-20991.herokuapp.com/");
-header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Origin: https://young-coast-20991.herokuapp.com/");
+// header('Access-Control-Allow-Origin: *'); 
 
 /**
  * Laravel - A PHP Framework For Web Artisans
