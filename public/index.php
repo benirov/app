ac7d87d82352aef6ab1d22322bcf01a6a388adea
Beniro <beniro_vielma@hotmail.com>
2018-07-25 21:54:31 -0500

    commit heroku laravel

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-
+// header("Access-Control-Allow-Origin: https://young-coast-20991.herokuapp.com/");
 // header('Access-Control-Allow-Origin: *'); 
 
 /**
