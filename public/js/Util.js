e025ec2de20c0544dc2ac1b56f68c8aefdf07180
Beniro <beniro_vielma@hotmail.com>
2018-07-27 23:33:13 -0500

    commit to app laravel in heroku

--- public/js/Util.js
+++ public/js/Util.js
@@ -741,7 +741,7 @@ $("#btnModalBinnacle").on("click", function()
         $.each(sResp, function(i, val)
         {
             $(".timeline").append('<li class="timeline-inverted">'+
-                                    '<div class="timeline-badge info"><i class="fa fa-icon"></i></div>'+
+                                    '<div class="timeline-badge info"><i class="fa fa-info"></i></div>'+
                                     '<div class="timeline-panel">'+
                                     '<div class="timeline-heading">'+
                                         '<h4 class="timeline-title">'+val.name+'</h4>'+
