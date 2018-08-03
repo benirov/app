ba7c406f7119b47797c42debfd30c67fdd48c205
Beniro <beniro_vielma@hotmail.com>
2018-08-03 15:56:02 -0500

    commit to app laravel in heroku

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -131,7 +131,7 @@
                                         <label for="txtchildren"><b>{{ trans('message.children') }}</b></label>
                                          <div class="input-group">
                                             <span class="input-group-addon"><i class="fas fa-child"></i></span>
-                                             <input id="txtchildren" type="number" class="form-control RegNumsimple" placeholder="{{ trans('message.children') }}" name="birth"/>
+                                             <input id="txtchildren" type="number" class="form-control RegNumsimple" placeholder="{{ trans('message.children') }}" name="children"/>
                                          </div>
                                         <span for="txtchildren" class="help-block"></span>
                                     </div>
