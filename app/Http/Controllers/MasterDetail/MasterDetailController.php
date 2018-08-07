006009c4250750019b096c50a45b8bd826062257
Beniro <beniro_vielma@hotmail.com>
2018-08-07 16:27:54 -0500

    commit heroku laravel

--- app/Http/Controllers/MasterDetail/MasterDetailController.php
+++ app/Http/Controllers/MasterDetail/MasterDetailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\MasterDetail;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 
 class MasterDetailController extends ApiController
 {
