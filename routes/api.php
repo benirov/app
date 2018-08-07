6fa03f6b45b1e7a6d8f101978e6eac8c878b157e
Beniro <beniro_vielma@hotmail.com>
2018-08-07 16:26:26 -0500

    commit heroku laravel

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,6 @@ Route::resource('company', 'Company\CompanyController');
 Route::resource('getUser', 'Auth\LoginController');
 
 Route::resource('masters', 'Master\MasterController');
-Route::resource('masterdetail', 'Master\MasterDetailController');
+Route::resource('masterdetail', 'MasterDetail\MasterDetailController');
 // Route::resource('Util', 'UtilController');
 
