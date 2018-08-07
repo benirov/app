22810db38c0525bebca487d98280184c7c91c490
Beniro <beniro_vielma@hotmail.com>
2018-08-07 14:10:06 -0500

    commit to app laravel in heroku

--- public/js/maestro.js
+++ public/js/maestro.js
@@ -9,9 +9,9 @@ function getMasters()
 	$.when(getQuery('masters', 'json', '', true, 1)).done(function (sRespMasters)
 		{
 			console.log(sRespMasters);
-			console.log(sRespMAsters[0]);
+			
 
-			$.each(sRespMAsters[0].data, function(i, val){
+			$.each(sRespMAsters.data, function(i, val){
 				$("#idmaster").append('<option value="'+val.id+'">'+val.name+'</option>');
 			});
 
