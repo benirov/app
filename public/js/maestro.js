c8532f9d9e4d2a0b1c71ac0acafceb73c7099866
Beniro <beniro_vielma@hotmail.com>
2018-08-08 15:44:24 -0500

    commit heroku laravel

--- public/js/maestro.js
+++ public/js/maestro.js
@@ -33,31 +33,23 @@ $(document).ready(function()
         { Name: "United Kingdom", Id: 3 }
     ];
 
-	$("#jsGrid").jsGrid({
-        height: "90%",
+	 $("#jsGrid").jsGrid({
         width: "100%",
+        height: "400px",
  
-        filtering: true,
+        inserting: true,
         editing: true,
         sorting: true,
         paging: true,
-        autoload: true,
  
-        pageSize: 15,
-        pageButtonCount: 5,
- 
-        deleteConfirm: "Do you really want to delete the client?",
- 
-        controller: db,
-
         data: clients,
  
         fields: [
-            { name: "Name", type: "text", width: 150 },
-            { name: "Age", type: "number", width: 50 },
-            { name: "Address", type: "text", width: 200 },
-            { name: "Country", type: "select", items: db.countries, valueField: "Id", textField: "Name" },
-            { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
+            { name: "Name", type: "text", width: 150,, sorting: true validate: "required" },
+            { name: "Age", type: "number", width: 50, sorting: true },
+            { name: "Address", type: "text", width: 200, sorting: true },
+            { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
+            { name: "Married", type: "checkbox", title: "Is Married", sorting: true },
             { type: "control" }
         ]
     });
