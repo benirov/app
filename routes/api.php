3a24fa18a61d983d07127d4efd136e24b84f7870
Beniro <beniro_vielma@hotmail.com>
2018-08-08 17:16:19 -0500

    commit heroku laravel

--- routes/api.php
+++ routes/api.php
@@ -17,14 +17,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
-Route::group(['prefix' => 'v1','middleware' => 'auth:api'], function () {
-    //    Route::resource('task', 'TasksController');
 
-    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
-    #adminlte_api_routes
-});
 
-Route::group(['middleware' => 'verifySession'], function () {
+Route::group(['middleware' => 'verifySession:api'], function () {
 	Route::resource('users', 'User\UserController');
 	Route::resource('company', 'Company\CompanyController');
 	Route::resource('getUser', 'Auth\LoginController');
