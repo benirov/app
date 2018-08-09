58ec4afb1e7ed7532f494e833df3913b20f90110
Beniro <beniro_vielma@hotmail.com>
2018-08-09 14:33:22 -0500

    commit to app laravel to laravel

--- public/js/maestro.js
+++ public/js/maestro.js
@@ -66,7 +66,30 @@ function drawGrid(Data) {
 
 	// console.log(Data);
 
-	
+	var db = {
+
+        loadData: function(filter) {
+            return $.grep(this.data, function(client) {
+                return (!filter.id || client.id.indexOf(filter.status) > -1)
+                    && (!filter.name || client.name === filter.name)
+                    && (!filter.status || client.status.indexOf(filter.status) > -1)
+            });
+        },
+
+        insertItem: function(insertingClient) {
+            this.clients.push(insertingClient);
+        },
+
+        updateItem: function(updatingClient) { },
+
+        deleteItem: function(deletingClient) {
+            var clientIndex = $.inArray(deletingClient, this.clients);
+            this.clients.splice(clientIndex, 1);
+        }
+
+    };
+
+    db.data = Data;
 
 	 $("#jsGrid").jsGrid({
         width: "100%",
@@ -80,9 +103,8 @@ function drawGrid(Data) {
         autoload: false,
 
 		 
-         data: Data,
-        controller: {
-            data:Data
+         // data: Data,
+        controller: db,
             // loadData: function (filter) {
             //     return $.grep(this.data, function (item) {
             //     	console.log(item);
@@ -92,7 +114,7 @@ function drawGrid(Data) {
             //         		(!filter.status || item.status.indexOf(filter.status) >= 0)
             //     });
             // },          
-        },
+        
  
         fields: [
             { name: "id", type: "text", width: 50, validate: "required", sorting: true, editing: false, },
