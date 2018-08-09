b8f5532123faf2605181a67859233288e837ba75
Beniro <beniro_vielma@hotmail.com>
2018-08-09 08:05:28 -0500

    commit to app laravel to laravel

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Session;
 
 $session = Session::get("sessionActive");
 // quede aqui la variable de sesion hay que pasarla fuera de comillas
-Route::group(['middleware' => 'verifySession:{$session}'], function () {
+Route::group(['middleware' => 'verifySession:'.$session], function () {
        
      Route::get('/home', 'HomeController@getHome')->name('home');
      Route::get('/maestro', 'HomeController@getViewMaster')->name('master');
