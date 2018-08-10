9efe9cc2dd7b5fffc1c9f307916041e977c6d2a8
Beniro <beniro_vielma@hotmail.com>
2018-08-10 17:51:00 -0500

    commit to app laravel to laravel

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Http\Request;
 use App\User;
 use App\Login;
