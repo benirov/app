8323d8cb9d897dca9bd1970e3bb2f9103d09e303
Beniro <beniro_vielma@hotmail.com>
2018-08-10 10:51:22 -0500

    commit to app laravel to heroku

--- public/plugins/jsgrid/jsgrid.js
+++ public/plugins/jsgrid/jsgrid.js
@@ -1267,25 +1267,25 @@
 
         _createEditRow: function(item) {
             console.log("esta es la funcion");
-            let divElement = document.createElement('<div class="modal fade" id="modalformEdit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"></div>');
-            let headElement = document.createElement('<div class="modal-dialog" role="document">'+
-                                    '<div class="modal-content">'+
-                                        '<div class="modal-header">'+
-                                            '<h5 class="modal-title" id="exampleModalLabel">Editing</h5>'+
-                                            '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
-                                                '<span aria-hidden="true">&times;</span>'+
-                                            '</button>'+
-                                        '</div>'+
-                                        '<div class="modal-body">'+
-                                            '<div class="container-fluid">'+
+            // let divElement = document.createElement('<div class="modal fade" id="modalformEdit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"></div>');
+            // let headElement = document.createElement('<div class="modal-dialog" role="document">'+
+            //                         '<div class="modal-content">'+
+            //                             '<div class="modal-header">'+
+            //                                 '<h5 class="modal-title" id="exampleModalLabel">Editing</h5>'+
+            //                                 '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
+            //                                     '<span aria-hidden="true">&times;</span>'+
+            //                                 '</button>'+
+            //                             '</div>'+
+            //                             '<div class="modal-body">'+
+            //                                 '<div class="container-fluid">'+
                                               
-                                            '</div>'+
-                                        '</div>'+
-                                        '<div class="modal-footer">'+
-                                            '<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'+
-                                        ' </div>'+
-                                    '</div>'+
-                                '</div>');
+            //                                 '</div>'+
+            //                             '</div>'+
+            //                             '<div class="modal-footer">'+
+            //                                 '<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'+
+            //                             ' </div>'+
+            //                         '</div>'+
+            //                     '</div>');
             //  divElement.appendChild(headElement);                                // Append the text to <button>
             // document.body.appendChild(divElement); 
             
