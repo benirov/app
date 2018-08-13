606e746c26a756b313ae54dc650c6a578a3730da
Develov <beniro_vielma@hotmail.com>
2018-08-13 14:20:33 -0500

    commit app laravel to heroku

--- app/Http/Controllers/Master/MasterController.php
+++ app/Http/Controllers/Master/MasterController.php
@@ -99,6 +99,7 @@ class MasterController extends ApiController
     }
 
     public function updateMaster(Request $request){
-        echo $request;
+        echo "aqui";
+        echo $request->id();
     }
 }
