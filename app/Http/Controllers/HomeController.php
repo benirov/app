34c60de95b007d82372c9be502b171b5a62f71d8
Develov <beniro_vielma@hotmail.com>
2018-08-14 12:25:38 -0500

    commit to app laravel to laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Cookie;
-use Custom\Grid;
+// use Custom\Grid;
 require_once('../resources/views/gridClass.php');
 use Session;
 /**
