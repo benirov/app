7015bed8c32e7481ad196e096c5c8b2442ba505f
Develov <beniro_vielma@hotmail.com>
2018-08-14 18:19:57 -0500

    commit to app laravel to laravel

--- resources/views/gridClass.php
+++ resources/views/gridClass.php
@@ -68,19 +68,19 @@ class GridClass extends MasterController
 
     }
 
-    public function renderFilter()
-    {
-        // $filterRender;
-        // if($this->filter)
-        // {
-            $filterRender  = '<div class="col-md-5">';
-            $filterRender .=     '<label for"find">searc</label>';
-            $filterRender   .=    '<input type="text" class="form-control" id="input" placeholder="search">';
-            $filterRender .= '</div">';
-        // }
+    // public function renderFilter()
+    // {
+    //     // $filterRender;
+    //     // if($this->filter)
+    //     // {
+    //         $filterRender  = '<div class="col-md-5">';
+    //         $filterRender .=     '<label for"find">searc</label>';
+    //         $filterRender   .=    '<input type="text" class="form-control" id="input" placeholder="search">';
+    //         $filterRender .= '</div">';
+    //     // }
 
-        return $filterRender;
-    }
+    //     return $filterRender;
+    // }
 
     
 
