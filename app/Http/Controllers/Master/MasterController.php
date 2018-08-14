37c3236f12481ccfc9ddc4940ae2e18a47808a92
Develov <beniro_vielma@hotmail.com>
2018-08-14 15:45:35 -0500

    commit to app laravel to laravel

--- app/Http/Controllers/Master/MasterController.php
+++ app/Http/Controllers/Master/MasterController.php
@@ -124,7 +124,7 @@ class MasterController extends ApiController
     public function getMaster()
     {
         $Master =  master::getDataMaster();
-        return $Master;
+        return $this->showAll($Master, 200);
 
     }
 }
