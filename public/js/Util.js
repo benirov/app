0ba10d9b539a0cd641bbcfd8065e93a9f8715b26
Develov <beniro_vielma@hotmail.com>
2018-08-14 11:23:40 -0500

    commit to app laravel to laravel

--- public/js/Util.js
+++ public/js/Util.js
@@ -7,67 +7,23 @@ $(document).ready(function()
     $('a').on('click', function() { inFormOrLink = true; });
     $('form').on('submit', function() { inFormOrLink = true; });
 
-    window.addEventListener("beforeunload", function (event) {
-  console.log(event);
-    // event.preventDefault();
-    if(!inFormOrLink)
+    window.addEventListener("beforeunload", function (event)
     {
+        if(!inFormOrLink)
+        {
 
-        $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+             });
+             $.ajax({
+                 type: 'POST',
+                 async: false,
+                 url: 'https://young-coast-20991.herokuapp.com/logout'
+               });
         }
-      });
-         $.ajax({
-             type: 'POST',
-             async: false,
-             url: 'https://young-coast-20991.herokuapp.com/logout'
-           });
-        
-        // return false;
-         // return 'Esta accion cerrara la ventana';
-
-
-        // sure("pruebas", function(sResp)
-        // {
-        //     if(sResp)
-        //     {
-        //         console.log("aqui");
-        //     }
-        // });
-
-        // event.returnValue = "null"; 
-    }
-    
-
-  // Chrome/Chromium based browsers still need this one.
-      // event.returnValue = "\o/";
     });
-//     var inFormOrLink;
-//     $('a').on('click', function() { inFormOrLink = true; });
-//     $('form').on('submit', function() { inFormOrLink = true; });
-
-//     $(window).on('beforeunload', function(eventObject) {
-//     var returnValue = undefined;
-//     if ((typeof(inFormOrLink) == 'undefined' && !InFormOrLink)) {
-//         returnValue = "Do you really want to close?";
-//         sure("cerrara la ventana", function(sResp)
-//         {
-//             if(sResp)
-//             {
-//                 console.log("aqui");
-//                 return false;
-//             }
-//         });
-//     }
-//     else
-//     {
-//         console.log("no");
-//         return false;
-//     }
-//     // eventObject.returnValue = returnValue;
-//     // return returnValue;
-// }); 
 });
 
 function alert(Title) {
